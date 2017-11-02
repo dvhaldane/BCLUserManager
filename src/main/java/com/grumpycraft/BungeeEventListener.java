a3fff370126d7be888341b239eed25dae0643524
dvhaldane <dvhaldane@me.com>
2017-11-02 16:11:49 -0500

    Rework

---
+++ src/main/java/com/grumpycraft/BungeeEventListener.java
@@ -0,0 +1,10 @@
+package com.grumpycraft;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Listener;
+
+public class BCLUserCommand implements Listener
+{
+
+    public onPosgt
+}
